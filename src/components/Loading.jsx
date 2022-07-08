841707c5721969bf4f1923ae8d87ec609b020901
HiepFK <hiepnh.fk@gmail.com>
2022-07-09 01:06:40 +0700

    done loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import loading from "../asset/loading.gif";
 import styled from "styled-components";
 function Loading() {
   return (
     <Wrapper>
-      <img src={loading} alt="" />;
+      <div className="loading" />
     </Wrapper>
   );
 }
@@ -12,10 +11,43 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-top: 5rem;
-  margin-bottom: 11rem;
-  img {
-    width: 20rem;
+  height: calc(100vh - 11rem);
+  .loading {
+    position: relative;
+    height: 5rem;
+  }
+  .loading:after,
+  .loading:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    border-radius: 5rem;
+    width: 5rem;
+    height: 5rem;
+  }
+
+  .loading:before {
+    z-index: 5;
+    border: 5px dashed white;
+    border-left: 5px solid transparent;
+    border-bottom: 5px solid transparent;
+    -webkit-animation: dashed 1s linear infinite;
+    animation: dashed 1s linear infinite;
+  }
+  .loading:after {
+    z-index: 10;
+    border: 5px solid white;
+    border-left: 5px solid transparent;
+    border-bottom: 5px solid transparent;
+    -webkit-animation: dashed 1s ease infinite;
+    animation: dashed 1s ease infinite;
+  }
+
+  @keyframes dashed {
+    to {
+      transform: rotate(360deg);
+    }
   }
 `;
 export default Loading;
